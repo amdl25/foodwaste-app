0268808204feca051c8b21db79fb00fa3b2c489e
Andreea Mihai <andreeamihai52@gmail.com>
2024-01-17 00:31:50 +0200

    Initial page structure

---
+++ aplicatiefoodwaste/frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (<div>
+        <h1 className='titlu'>Aplicatie food waste</h1>
+        <h2 className='about'>Realizat de: Mihai Andreea-Madalina, Margalina Mariana Narcisa</h2>
+      
+      </div>);
+}
